f659d73abd03640a98283b57fedb304142b65276
90 <hi@90.gripe>
2023-08-22 12:46:30 +0100

    Spotless et al

--- src/data/java/gripe/_90/arseng/data/DocumentationProvider.java
+++ src/data/java/gripe/_90/arseng/data/DocumentationProvider.java
@@ -1,5 +1,7 @@
 package gripe._90.arseng.data;
 
+import java.nio.file.Path;
+
 import com.hollingsworth.arsnouveau.common.datagen.PatchouliProvider;
 import com.hollingsworth.arsnouveau.common.datagen.patchouli.ApparatusPage;
 import com.hollingsworth.arsnouveau.common.datagen.patchouli.CraftingPage;
@@ -15,14 +17,11 @@ import net.minecraft.world.level.ItemLike;
 import gripe._90.arseng.definition.ArsEngBlocks;
 import gripe._90.arseng.definition.ArsEngItems;
 
-import java.nio.file.Path;
-
 @SuppressWarnings("deprecation")
 public class DocumentationProvider extends PatchouliProvider {
     public DocumentationProvider(DataGenerator generator) {
         super(generator);
     }
-
 
     @Override
     public PatchouliBuilder buildBasicItem(ItemLike item, ResourceLocation category, IPatchouliPage recipePage) {
@@ -45,12 +44,15 @@ public class DocumentationProvider extends PatchouliProvider {
 
     @Override
     public Path getPath(ResourceLocation category, ResourceLocation fileName) {
-        return this.generator.getOutputFolder().resolve("data/arseng/patchouli_books/worn_notebook/en_us/entries/" + category.getPath() + "/" + fileName.getPath() + ".json");
+        return getPath(category, fileName.getPath());
     }
 
     @Override
     public Path getPath(ResourceLocation category, String fileName) {
-        return this.generator.getOutputFolder().resolve("data/arseng/patchouli_books/worn_notebook/en_us/entries/" + category.getPath() + "/" + fileName + ".json");
+        return generator
+                .getOutputFolder()
+                .resolve("data/arseng/patchouli_books/worn_notebook/en_us/entries/%s/%s.json"
+                        .formatted(category.getPath(), fileName));
     }
 
     @Override
@@ -58,7 +60,8 @@ public class DocumentationProvider extends PatchouliProvider {
         var sourceAcceptorBuilder = buildBasicItem(
                 ArsEngBlocks.SOURCE_ACCEPTOR, AUTOMATION, new ApparatusPage(ArsEngBlocks.SOURCE_ACCEPTOR));
         sourceAcceptorBuilder.withPage(new TextPage(getLangPath("source_acceptor_description")));
-        sourceAcceptorBuilder.withPage(new CraftingPage("arseng:source_acceptor_part"));//name must match that of the recipe
+        sourceAcceptorBuilder.withPage(
+                new CraftingPage("arseng:source_acceptor_part")); // name must match that of the recipe
         addPage(new PatchouliPage(
                 sourceAcceptorBuilder,
                 getPath(AUTOMATION, Registry.ITEM.getKey(ArsEngBlocks.SOURCE_ACCEPTOR.asItem()))));
