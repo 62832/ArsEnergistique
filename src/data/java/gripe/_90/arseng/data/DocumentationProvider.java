b8561ba59cd4d965afcde5e8283b83492c93ff7e
dkmk100 <33072952+dkmk100@users.noreply.github.com>
2023-08-21 17:26:38 -0700

    fix datagen
    I fixed the worn notebook entries not showing up, and moved the enchanting apparatus recipes to the right place.

--- src/data/java/gripe/_90/arseng/data/DocumentationProvider.java
+++ src/data/java/gripe/_90/arseng/data/DocumentationProvider.java
@@ -15,11 +15,14 @@ import net.minecraft.world.level.ItemLike;
 import gripe._90.arseng.definition.ArsEngBlocks;
 import gripe._90.arseng.definition.ArsEngItems;
 
+import java.nio.file.Path;
+
 @SuppressWarnings("deprecation")
 public class DocumentationProvider extends PatchouliProvider {
     public DocumentationProvider(DataGenerator generator) {
         super(generator);
     }
+
 
     @Override
     public PatchouliBuilder buildBasicItem(ItemLike item, ResourceLocation category, IPatchouliPage recipePage) {
@@ -41,11 +44,21 @@ public class DocumentationProvider extends PatchouliProvider {
     }
 
     @Override
+    public Path getPath(ResourceLocation category, ResourceLocation fileName) {
+        return this.generator.getOutputFolder().resolve("data/arseng/patchouli_books/worn_notebook/en_us/entries/" + category.getPath() + "/" + fileName.getPath() + ".json");
+    }
+
+    @Override
+    public Path getPath(ResourceLocation category, String fileName) {
+        return this.generator.getOutputFolder().resolve("data/arseng/patchouli_books/worn_notebook/en_us/entries/" + category.getPath() + "/" + fileName + ".json");
+    }
+
+    @Override
     public void addEntries() {
         var sourceAcceptorBuilder = buildBasicItem(
                 ArsEngBlocks.SOURCE_ACCEPTOR, AUTOMATION, new ApparatusPage(ArsEngBlocks.SOURCE_ACCEPTOR));
         sourceAcceptorBuilder.withPage(new TextPage(getLangPath("source_acceptor_description")));
-        sourceAcceptorBuilder.withPage(new CraftingPage(ArsEngItems.SOURCE_ACCEPTOR_PART));
+        sourceAcceptorBuilder.withPage(new CraftingPage("arseng:source_acceptor_part"));//name must match that of the recipe
         addPage(new PatchouliPage(
                 sourceAcceptorBuilder,
                 getPath(AUTOMATION, Registry.ITEM.getKey(ArsEngBlocks.SOURCE_ACCEPTOR.asItem()))));
