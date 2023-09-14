b365f0bfb7eba7bb3a038171b63a37194b1a45cd
90 <hi@90.gripe>
2023-09-14 19:50:07 +0100

    Fix client crash when displaying source in a storage monitor Closes #4

--- src/main/java/gripe/_90/arseng/me/client/SourceRenderer.java
+++ src/main/java/gripe/_90/arseng/me/client/SourceRenderer.java
@@ -33,58 +33,58 @@ public class SourceRenderer implements IAEStackRenderHandler<SourceKey> {
                 .blit(poseStack, 100 + zIndex);
     }
 
+    @SuppressWarnings("resource")
     @Override
     public void drawOnBlockFace(
             PoseStack poseStack, MultiBufferSource buffers, SourceKey what, float scale, int combinedLight) {
-        try (var sprite = SOURCE.sprite()) {
-            poseStack.pushPose();
-            // Push it out of the block face a bit to avoid z-fighting
-            poseStack.translate(0, 0, 0.01f);
+        var sprite = SOURCE.sprite();
+        poseStack.pushPose();
+        // Push it out of the block face a bit to avoid z-fighting
+        poseStack.translate(0, 0, 0.01f);
 
-            var buffer = buffers.getBuffer(RenderType.solid());
+        var buffer = buffers.getBuffer(RenderType.solid());
 
-            // In comparison to items, make it _slightly_ smaller because item icons
-            // usually don't extend to the full size.
-            scale -= 0.05f;
+        // In comparison to items, make it _slightly_ smaller because item icons
+        // usually don't extend to the full size.
+        scale -= 0.05f;
 
-            // y is flipped here
-            var x0 = -scale / 2;
-            var y0 = scale / 2;
-            var x1 = scale / 2;
-            var y1 = -scale / 2;
+        // y is flipped here
+        var x0 = -scale / 2;
+        var y0 = scale / 2;
+        var x1 = scale / 2;
+        var y1 = -scale / 2;
 
-            var transform = poseStack.last().pose();
-            buffer.vertex(transform, x0, y1, 0)
-                    .color(-1)
-                    .uv(sprite.getU0(), sprite.getV1())
-                    .overlayCoords(OverlayTexture.NO_OVERLAY)
-                    .uv2(combinedLight)
-                    .normal(0, 0, 1)
-                    .endVertex();
-            buffer.vertex(transform, x1, y1, 0)
-                    .color(-1)
-                    .uv(sprite.getU1(), sprite.getV1())
-                    .overlayCoords(OverlayTexture.NO_OVERLAY)
-                    .uv2(combinedLight)
-                    .normal(0, 0, 1)
-                    .endVertex();
-            buffer.vertex(transform, x1, y0, 0)
-                    .color(-1)
-                    .uv(sprite.getU1(), sprite.getV0())
-                    .overlayCoords(OverlayTexture.NO_OVERLAY)
-                    .uv2(combinedLight)
-                    .normal(0, 0, 1)
-                    .endVertex();
-            buffer.vertex(transform, x0, y0, 0)
-                    .color(-1)
-                    .uv(sprite.getU0(), sprite.getV0())
-                    .overlayCoords(OverlayTexture.NO_OVERLAY)
-                    .uv2(combinedLight)
-                    .normal(0, 0, 1)
-                    .endVertex();
+        var transform = poseStack.last().pose();
+        buffer.vertex(transform, x0, y1, 0)
+                .color(-1)
+                .uv(sprite.getU0(), sprite.getV1())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x1, y1, 0)
+                .color(-1)
+                .uv(sprite.getU1(), sprite.getV1())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x1, y0, 0)
+                .color(-1)
+                .uv(sprite.getU1(), sprite.getV0())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
+        buffer.vertex(transform, x0, y0, 0)
+                .color(-1)
+                .uv(sprite.getU0(), sprite.getV0())
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(combinedLight)
+                .normal(0, 0, 1)
+                .endVertex();
 
-            poseStack.popPose();
-        }
+        poseStack.popPose();
     }
 
     @Override
