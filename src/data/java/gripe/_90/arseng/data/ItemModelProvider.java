95b5793af628ad59e640dbf832dc694f56d3d86e
Ridanis <khaled.chikhou@gmail.com>
2024-05-24 21:55:30 +0200

    Re-Textures

--- src/data/java/gripe/_90/arseng/data/ItemModelProvider.java
+++ src/data/java/gripe/_90/arseng/data/ItemModelProvider.java
@@ -23,31 +23,38 @@ class ItemModelProvider extends net.neoforged.neoforge.client.model.generators.I
 
         ArsEngItems.getCells()
                 .forEach(cell -> flatSingleLayer(cell).texture("layer1", AppEng.makeId("item/storage_cell_led")));
-        ArsEngItems.getPortables()
-                .forEach(cell -> flatSingleLayer(cell).texture("layer1", AppEng.makeId("item/portable_cell_led")));
+        ArsEngItems.getPortables().forEach(this::portable);
 
         var p2pTunnelItem = AppEng.makeId("item/p2p_tunnel_base");
         var p2pTunnelPart = AppEng.makeId("part/p2p/p2p_tunnel_base");
-        var sourceGem = ResourceLocation.fromNamespaceAndPath("ars_nouveau", "block/source_gem_block");
-        var sourceStone = ResourceLocation.fromNamespaceAndPath("ars_nouveau", "block/gilded_sourcestone_large_bricks");
-        withExistingParent("item/source_p2p_tunnel", p2pTunnelItem).texture("type", sourceGem);
-        withExistingParent("part/source_p2p_tunnel", p2pTunnelPart).texture("type", sourceGem);
-        withExistingParent("item/spell_p2p_tunnel", p2pTunnelItem).texture("type", sourceStone);
-        withExistingParent("part/spell_p2p_tunnel", p2pTunnelPart).texture("type", sourceStone);
+        var sourceP2P = ArsEnergistique.makeId("part/p2p_tunnel_source");
+        var spellP2P = ArsEnergistique.makeId("part/p2p_tunnel_spell");
+        withExistingParent("item/source_p2p_tunnel", p2pTunnelItem).texture("type", sourceP2P);
+        withExistingParent("part/source_p2p_tunnel", p2pTunnelPart).texture("type", sourceP2P);
+        withExistingParent("item/spell_p2p_tunnel", p2pTunnelItem).texture("type", spellP2P);
+        withExistingParent("part/spell_p2p_tunnel", p2pTunnelPart).texture("type", spellP2P);
 
         withExistingParent("part/source_acceptor", AppEng.makeId("part/energy_acceptor"))
-                .texture("sides", "part/source_acceptor_sides")
-                .texture("back", "part/source_acceptor_back")
                 .texture("front", "block/source_acceptor")
-                .texture("particle", "part/source_acceptor_back");
         withExistingParent("item/cable_source_acceptor", AppEng.makeId("item/cable_energy_acceptor"))
-                .texture("sides", "part/source_acceptor_sides")
-                .texture("back", "part/source_acceptor_back")
                 .texture("front", "block/source_acceptor");
     }
 
     private ItemModelBuilder flatSingleLayer(ItemDefinition<?> item) {
         var path = item.id().getPath();
         return singleTexture(path, mcLoc("item/generated"), "layer0", ArsEnergistique.makeId("item/" + path));
+    }
+
+    private void portable(ItemDefinition<?> portable) {
+        var id = portable.id().getPath();
+        var tierSuffix = id.substring(id.lastIndexOf('_'));
+
+        singleTexture(
+                        id,
+                        mcLoc("item/generated"),
+                        "layer0", ArsEnergistique.makeId("item/portable_source_cell_housing"))
+                .texture("layer1", AppEng.makeId("item/portable_cell_led"))
+                .texture("layer2", ArsEnergistique.makeId("item/portable_cell_screen"))
+                .texture("layer3", ArsEnergistique.makeId("item/portable_source_cell" + tierSuffix));
     }
 }
