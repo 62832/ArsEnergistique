9467405592aa1ddba899c7aee818ce8b2b029235
90 <hi@90.gripe>
2024-06-02 15:45:40 +0100

    buff default Source → AE energy conversion to 16AE

--- src/main/java/gripe/_90/arseng/definition/ArsEngConfig.java
+++ src/main/java/gripe/_90/arseng/definition/ArsEngConfig.java
@@ -14,7 +14,7 @@ public final class ArsEngConfig {
         var builder = new ForgeConfigSpec.Builder();
 
         AE_PER_SOURCE = builder.comment("How much AE energy the Source Acceptor should produce per unit of source.")
-                .defineInRange("ae_per_source", 2, 0, Double.MAX_VALUE);
+                .defineInRange("ae_per_source", 16, 0, Double.MAX_VALUE);
         OUTPUT_P2P_BUFFER = builder.comment("The maximum source capacity of Source P2P output tunnels.")
                 .defineInRange("output_p2p_buffer", 1000, 0, Integer.MAX_VALUE);
 
