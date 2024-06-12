e12dee9fcf494fa6272747486821f982e2192936
90 <hi@90.gripe>
2024-06-12 17:04:47 +0100

    Fix #21: View cells only accept one item at a time, config inventories do not persist between reloads

--- src/main/java/gripe/_90/arseng/mixin/ConfigInventoryMixin.java
+++ src/main/java/gripe/_90/arseng/mixin/ConfigInventoryMixin.java
@@ -2,10 +2,11 @@ package gripe._90.arseng.mixin;
 
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import appeng.api.stacks.GenericStack;
@@ -21,24 +22,17 @@ public abstract class ConfigInventoryMixin extends GenericStackInv {
         super(listener, size);
     }
 
+    @Shadow
+    @Nullable
+    public abstract GenericStack getStack(int slot);
+
     @Inject(method = "setStack", at = @At("HEAD"))
     private void rememberStack(int slot, GenericStack stack, CallbackInfo ci) {
         arseng$toStock = stack;
     }
 
-    // spotless:off
-    @Redirect(
-            method = "setStack",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lappeng/helpers/externalstorage/GenericStackInv;setStack(ILappeng/api/stacks/GenericStack;)V"))
-    // spotless:on
-    private void setForEmpty(GenericStackInv instance, int slot, GenericStack stack) {
-        if (arseng$toStock != null && arseng$toStock.amount() <= 0) {
-            var currentStack = instance.getStack(slot);
-            stack = currentStack == null ? new GenericStack(arseng$toStock.what(), 1) : null;
-        }
-
-        super.setStack(slot, stack);
+    @ModifyVariable(method = "setStack", at = @At(value = "STORE", ordinal = 1), argsOnly = true)
+    private GenericStack handleEmptyContainer(GenericStack stack, int slot) {
+        return getStack(slot) == null && arseng$toStock != null ? new GenericStack(arseng$toStock.what(), 1) : null;
     }
 }
