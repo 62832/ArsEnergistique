dba57d10dc018403c94a443bd1c8f79ccf5b634f
Jarva <4622609+Jarva@users.noreply.github.com>
2025-01-11 08:55:55 +0000

    chore: run spotless

--- src/main/java/gripe/_90/arseng/mixin/WixieCauldronTileMixin.java
+++ src/main/java/gripe/_90/arseng/mixin/WixieCauldronTileMixin.java
@@ -1,5 +1,21 @@
 package gripe._90.arseng.mixin;
 
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.hollingsworth.arsnouveau.common.block.tile.SummoningTile;
+import com.hollingsworth.arsnouveau.common.block.tile.WixieCauldronTile;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
 import appeng.api.crafting.IPatternDetails;
 import appeng.api.crafting.PatternDetailsHelper;
 import appeng.api.inventories.InternalInventory;
@@ -7,19 +23,6 @@ import appeng.api.stacks.AEItemKey;
 import appeng.api.stacks.AEKey;
 import appeng.api.stacks.GenericStack;
 import appeng.helpers.patternprovider.PatternContainer;
-import com.hollingsworth.arsnouveau.common.block.tile.SummoningTile;
-import com.hollingsworth.arsnouveau.common.block.tile.WixieCauldronTile;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.List;
 
 @Mixin(value = WixieCauldronTile.class, remap = false)
 public abstract class WixieCauldronTileMixin extends SummoningTile {
@@ -27,24 +30,31 @@ public abstract class WixieCauldronTileMixin extends SummoningTile {
         super(type, pos, state);
     }
 
-@ModifyExpressionValue(method = "rotateCraft", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBlockEntity(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/entity/BlockEntity;"), remap = false)
-private BlockEntity checkBlockEntity(BlockEntity be, @Local List<ItemStack> craftables) {
-    if (!(be instanceof PatternContainer networked)) return be;
+    @ModifyExpressionValue(
+            method = "rotateCraft",
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target =
+                                    "Lnet/minecraft/world/level/Level;getBlockEntity(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/entity/BlockEntity;"),
+            remap = false)
+    private BlockEntity checkBlockEntity(BlockEntity be, @Local List<ItemStack> craftables) {
+        if (!(be instanceof PatternContainer networked)) return be;
 
-    InternalInventory inv = networked.getTerminalPatternInventory();
-    for (ItemStack stack : inv) {
-        if (!PatternDetailsHelper.isEncodedPattern(stack)) continue;
+        InternalInventory inv = networked.getTerminalPatternInventory();
+        for (ItemStack stack : inv) {
+            if (!PatternDetailsHelper.isEncodedPattern(stack)) continue;
 
-        IPatternDetails details = PatternDetailsHelper.decodePattern(stack, be.getLevel());
-        if (details == null) continue;
-        for (GenericStack output : details.getOutputs()) {
-            AEKey key = output.what();
-            if (key instanceof AEItemKey itemKey) {
-                craftables.add(itemKey.toStack(Math.toIntExact(output.amount())));
+            IPatternDetails details = PatternDetailsHelper.decodePattern(stack, be.getLevel());
+            if (details == null) continue;
+            for (GenericStack output : details.getOutputs()) {
+                AEKey key = output.what();
+                if (key instanceof AEItemKey itemKey) {
+                    craftables.add(itemKey.toStack(Math.toIntExact(output.amount())));
+                }
             }
         }
-    }
 
-    return be;
-}
+        return be;
+    }
 }
